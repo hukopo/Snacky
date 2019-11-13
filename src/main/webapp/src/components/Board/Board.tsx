6155c6ab707610738ee3379f3336099b0d1d3750
holkin <holkin@skbkontur>
2019-11-14 00:59:18 +0500

    настроил прокси для реакта, сделал вебкпак билд в продакшн. начал апи бейз на клиенте и поверстал чуток

--- src/main/webapp/src/components/Board/Board.tsx
+++ src/main/webapp/src/components/Board/Board.tsx
@@ -1,11 +1,27 @@
 import * as React from "react";
 
 import cn from './Board.less';
+import { Card } from "../Card/Card";
 
 export class Board extends React.Component {
     render() {
         return (
             <div className={cn("board")}>
+                <input/>
+                <div className={cn("cards")}>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+                <Card/>
+
+                </div>
             </div>
         );
     }
